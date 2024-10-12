8e446da531eb42f3596a76a533260500487d9b5a
Saiyed Gulamali <91742459+Saiyed-Gulamali@users.noreply.github.com>
2024-10-12 01:13:29 -0400

    Update saiyed.php

--- saiyed.php
+++ saiyed.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Caf&eacute;!</title>
@@ -12,6 +12,7 @@
 		<hr>
 		<div class="center"> Lastname Caf&eacute;</div>
 	</div>
+
 	<br>
 	<hr>
 	<div class="topnav">
@@ -20,53 +21,52 @@
 		<a href="#contactUs">Contact Us</a>
 	</div>
 	<hr>
-	<div id="mainContent">
 
+	<div id="mainContent">
 		<div id="mainPictures" class="center">
 			<table>
 				<tr>
-					<td><img src="images/Coffee-and-Pastries.jpg" height=auto width="490"></td>
-					<td><img src="images/Cake-Vitrine.jpg" height=auto width="450"></td>
+					<td><img src="images/Coffee-and-Pastries.jpg" width="490" alt="Coffee and Pastries"></td>
+					<td><img src="images/Cake-Vitrine.jpg" width="450" alt="Cake Vitrine"></td>
 				</tr>
 			</table>
+
 			<hr>
-			<div id="header" class="mainHeader"><p>Gulamali Saiyed's Caf&eacute; Employee List!</p></div>
+			<div id="header" class="mainHeader">
+				<p>Lastname Caf&eacute; Employee List!</p>
+			</div>
 			<br>
 
-            <?php
-            // Connection details
-            $connection_string = "host=saiyed-instance-1.c5ywmyoi8flm.ca-central-1.rds.amazonaws.com port=5432 dbname=saiyed user=postgres password=Night0220";
+			<?php
+			// Database connection settings
+			$connection_string = "host=saiyed.c5ywmyoi8flm.ca-central-1.rds.amazonaws.com port=5432 dbname=saiyed user=postgres password=Night0220";
 
-            // Try to establish a connection
-            $connection = pg_connect($connection_string);
+			// Establish connection to PostgreSQL database
+			$connection = pg_connect($connection_string);
+			if (!$connection) {
+				die("Could not connect to the database: " . pg_last_error());
+			}
 
-           
+			// Query to retrieve employee data
+			$query = "SELECT * FROM employee";
+			$result = pg_query($connection, $query);
 
-            // Fetch employee data (if connected successfully)
-            $query = "SELECT * FROM employee";
-            $result = pg_query($connection, $query);
+			if (!$result) {
+				die("Error reading data: " . pg_last_error());
+			}
 
-            if ($result) {
-                echo "<table border='1'>";
-                echo "<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Position</th><th>Created At</th></tr>";
-                while ($row = pg_fetch_assoc($result)) {
-                    echo "<tr>";
-                    echo "<td>" . $row['id'] . "</td>";
-                    echo "<td>" . $row['fname'] . "</td>";
-                    echo "<td>" . $row['lname'] . "</td>";
-                    echo "<td>" . $row['position'] . "</td>";
-                    echo "<td>" . $row['created_at'] . "</td>";
-                    echo "</tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "<p>Error fetching data: " . pg_last_error() . "</p>";
-            }
-            // Close the connection
-            pg_close($connection);
-            ?>
+			// Display employee data
+			while ($row = pg_fetch_assoc($result)) {
+				echo "<p>ID: " . htmlspecialchars($row['id']) .
+					", First Name: " . htmlspecialchars($row['fname']) .
+					", Last Name: " . htmlspecialchars($row['lname']) .
+					", Timestamp: " . htmlspecialchars($row['created_at']) . "</p>";
+			}
 
+			// Close the database connection
+			pg_close($connection);
+			?>
 		</div>
 	</div>
-</body> 
+</body>
 </html>
